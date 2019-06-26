b0d3be9189e6dfe16493ec3d26d2fa330f790355
ibrhalil <52247284+ibrhalil@users.noreply.github.com>
2019-06-26 16:54:54 +0300

    Add files via upload

---
+++ GirilenSayiyaKadarToplamlari.java
@@ -0,0 +1,18 @@
+package baslangicJavaProjeleri;
+
+import java.util.Scanner;													
+public class GirilenSayiyaKadarToplamlari 
+{
+	public static void main(String[] args) 
+	{
+		Scanner oku = new Scanner(System.in);								
+		System.out.print("Sayý Giriniz :");									
+		int GirilenSayi = oku.nextInt();									
+		int ToplamTut = 0;													
+		for (int i = 0; i <= GirilenSayi; i++) 								
+		{
+			ToplamTut+=i;													
+		}
+		System.out.print("Girilen Sayýya Kadar Toplamlarý : "+ ToplamTut);	
+	}
+}
