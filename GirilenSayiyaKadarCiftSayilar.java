6c555ada0d97f8b54decf620840f2ea9515ed126
ibrhalil <52247284+ibrhalil@users.noreply.github.com>
2019-06-26 19:16:34 +0300

    Add files via upload

---
+++ GirilenSayiyaKadarCiftSayilar.java
@@ -0,0 +1,22 @@
+package baslangicJavaProjeleri;
+
+import java.util.Scanner;
+
+public class GirilenSayiyaKadarCiftSayilar 
+{
+	public static void main(String[] args) 
+	{
+		Scanner oku = new Scanner(System.in);
+		System.out.print("Sayi Giriniz : ");
+		int girilenSayi = oku.nextInt();
+		int say = 0;
+		while (say<=girilenSayi) 
+		{
+			if (say%2==0) 
+			{
+				System.out.print(say+ " ");
+			}
+			say++;
+		}
+	}
+}
