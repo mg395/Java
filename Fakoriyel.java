c18dc9951cb4dde67c0fcec0a6d52c180f5c7eba
ibrhalil <52247284+ibrhalil@users.noreply.github.com>
2019-06-27 15:48:06 +0300

    Add files via upload

---
+++ Fakoriyel.java
@@ -0,0 +1,22 @@
+package baslangicJavaProjeleri;
+
+import java.util.Scanner;
+
+public class Fakoriyel 
+{
+	public static void main(String[] args) 
+	{
+		Scanner oku = new Scanner(System.in);
+		System.out.print("Sayi Giriniz :");
+		System.out.print(fak(oku.nextInt()));
+	}
+	private static int fak(int sayi) 
+	{
+		int fak=1;;
+		for (int i = 1; i <= sayi; i++) 
+		{
+			fak*=i;
+		}
+		return fak;
+	}
+}
